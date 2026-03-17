4416da7a90134e487ac19f85e79972a0934d58a9
Tanbir <57638199+tanbiralam@users.noreply.github.com>
2026-03-17 22:25:17 +0530

    Update 1186.cpp

--- 1186.cpp
+++ 1186.cpp
@@ -3,14 +3,26 @@ public:
     int maximumSum(vector<int>& arr) {
         int n = arr.size();
         int noDelete = arr[0];
-        int oneDelete = 0;
-        int ans = arr[0];
+        int oneDelete = INT_MIN;
+        int res = arr[0];
 
         for (int i = 1; i < n; i++) {
-            oneDelete = max(oneDelete + arr[i], noDelete);
-            noDelete = max(arr[i] + noDelete, arr[i]);
-            ans = max(ans, max(oneDelete, noDelete));
+            int prevNoDelete = noDelete;
+            int prevOneDelete = oneDelete;
+
+            noDelete = max(noDelete + arr[i], arr[i]);
+
+            int v2;
+
+            if (prevOneDelete == INT_MIN) {
+                v2 = arr[i];
+            } else {
+                v2 = prevOneDelete + arr[i];
+            }
+
+            oneDelete = max(v2, prevNoDelete);
+            res = max(res, max(oneDelete, noDelete));
         }
-        return ans;
+        return res;
     }
 };
